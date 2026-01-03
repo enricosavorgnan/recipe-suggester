a6f784aa63bc246998e4218345ebd28b93836fa9
tommaso <tommaso.atletica@gmail.com>
2026-01-03 11:56:30 +0100

    chore: initialize frontend (React)

---
+++ code/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import HealthCheck from './components/HealthCheck'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe Suggester</h1>
+      <HealthCheck />
+    </div>
+  )
+}
+
+export default App
